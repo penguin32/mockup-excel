3f0440d941c6ad2fee6206bd8fdce7bb4965f517
penguin32 <johnmarvinpangilinan258@gmail.com>
2024-08-14 22:18:33 +0800

    Adding function and tables color inside a table insert, finished.

--- interface/spreadsheetArea.lua
+++ interface/spreadsheetArea.lua
@@ -25,7 +25,7 @@ function spreadsheetArea.load()
 	spreadsheetArea.rRect={} -- rowRectangles, an Object that has value and positions.
 	spreadsheetArea.rAndC={} -- The table, rows and columns of spreadsheet.
 	spreadsheetArea.amountOfRows = 30	-- Could be change by user.
-	for i = 65,90,1 do -- Alphabets
+	for i = 65,90,1 do -- Alphabets , Columns.
 		table.insert(spreadsheetArea.cRect,{
 			value=string.char(i),
 			width=65,
@@ -36,7 +36,7 @@ function spreadsheetArea.load()
 			deltaY=spreadsheetArea.y
 		})
 	end
-	for i = 1,spreadsheetArea.amountOfRows,1 do
+	for i = 1,spreadsheetArea.amountOfRows,1 do -- Numbers, Rows
 		table.insert(spreadsheetArea.rRect,{
 			value=i,
 			width=spreadsheetArea.cBoxField.width,
@@ -50,25 +50,42 @@ function spreadsheetArea.load()
 	for i = 1,spreadsheetArea.amountOfRows,1 do
 		for j = 65,90,1 do -- Alphabets
 			table.insert(spreadsheetArea.rAndC,{
-				value="",
+				value=i..", "..j,
 				width=65,
 				height=20,
 	              x=spreadsheetArea.cBoxField.x+spreadsheetArea.cBoxField.width+65*(j-65),
 		      y=spreadsheetArea.cBoxField.y+spreadsheetArea.cBoxField.height+20*(i-1),
 		  deltaX=spreadsheetArea.cBoxField.x+spreadsheetArea.cBoxField.width+65*(i-65),
-	           deltaY=spreadsheetArea.cBoxField.y+spreadsheetArea.cBoxField.height+20*(i-1)
+	          deltaY=spreadsheetArea.cBoxField.y+spreadsheetArea.cBoxField.height+20*(i-1),
+		  ["color"]={["r"]=0,["g"]=0.1,["b"]=0},
+		  highlight = function(self)
+			if cursor.x > self.deltaX and cursor.y > self.deltaY and cursor.x < self.deltaX + self.width and cursor.y < self.deltaY + self.height then
+				self["color"]["g"]=0.1
+	      love.graphics.setColor(self["color"]["r"],self["color"]["g"],self["color"]["b"])
+		love.graphics.rectangle("fill",self.deltaX,self.deltaY,self.width,self.height)
+	      love.graphics.setColor(1,1,1)
+				self["color"]["g"]=1
+	      love.graphics.setColor(self["color"]["r"],self["color"]["g"],self["color"]["b"])
+		love.graphics.rectangle("line",self.deltaX,self.deltaY,self.width,self.height)
+	      love.graphics.setColor(1,1,1)
+			else
+				self["color"]["g"]=0.1
+	      love.graphics.setColor(self["color"]["r"],self["color"]["g"],self["color"]["b"])
+		love.graphics.rectangle("line",self.deltaX,self.deltaY,self.width,self.height)
+	      love.graphics.setColor(1,1,1)
+			end
+		  end
 			})
 		end
 	end
-
 	spreadsheetArea.getTotalLengthColumnRow() -- Used for scrollBar navigation.
 						  -- scrollBarUpdate()
 end
 
 function spreadsheetArea.draw()
-	for i,v in ipairs(spreadsheetArea.rAndC) do
-		love.graphics.rectangle("line",v.deltaX,v.deltaY,v.width,v.height)
-		love.graphics.print(v.value,v.deltaX+v.width/2,v.deltaY+v.height/6,0,1,1,string.len(v.value)*3)
+	for i = #spreadsheetArea.rAndC, 1, -1 do -- Iterating from the end of the sequence.
+		spreadsheetArea.rAndC[i].highlight(spreadsheetArea.rAndC[i])
+		love.graphics.print(spreadsheetArea.rAndC[i].value,spreadsheetArea.rAndC[i].deltaX+spreadsheetArea.rAndC[i].width/2,spreadsheetArea.rAndC[i].deltaY+spreadsheetArea.rAndC[i].height/6,0,1,1,string.len(spreadsheetArea.rAndC[i].value)*3)
 	end
 
 	for i,v in ipairs(spreadsheetArea.cRect) do
